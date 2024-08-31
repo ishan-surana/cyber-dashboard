165415c46deb86e026f05fe333658985f48114a4
ishan-surana <ishan5.mitmpl2022@learner.manipal.edu>
2024-08-31 10:05:57 +0530

    Configured map plotting for State drill-down

--- dashboard.py
+++ dashboard.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import streamlit as st
 import altair as alt
+import geopandas as gpd
+import matplotlib.pyplot as plt
 
 # Load data
 df = pd.read_csv('cyber_incidents.csv')
 features = df.drop(columns=['Date', 'Impact']).columns.to_list()
+india_map = gpd.read_file('map/Indian_states.shp')
 
 # Dashboard Title
 st.markdown("""
@@ -88,8 +91,10 @@ for tab, tab_name in zip(tabs, tab_names):
         if st.button(f"Toggle Select 'All {tab_name}s'"):
             if st.session_state[f"{drill_down_basis}_select_all"]:
                 selected_options = df[drill_down_basis].unique().tolist()
+                st.write("All selected toggled!")
             else:
                 selected_options = []
+                st.write("All deselected toggled!")
             st.session_state[f"{drill_down_basis}_selected_options"] = selected_options
             st.session_state[f"{drill_down_basis}_select_all"] = not st.session_state[f"{drill_down_basis}_select_all"]
 
@@ -113,6 +118,31 @@ for tab, tab_name in zip(tabs, tab_names):
                 y='Counts:Q',
                 tooltip=['Month', 'Counts']
             ).properties(width=700, height=300)
+        elif drill_down_basis == 'State':
+            fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+            india_map.boundary.plot(ax=ax, color='black', facecolor='none')
+            try:
+                india_map.plot(ax=ax, color='white', facecolor='none')
+                merged_map = india_map.merge(filtered_df.rename(columns={'State': 'state'}).groupby('state').size().reset_index(name='Impact'), on='state').plot(ax=ax, column='Impact', cmap='YlOrRd', legend=False)
+                # Create a colorbar
+                sm = plt.cm.ScalarMappable(cmap='YlOrRd', norm=plt.Normalize(vmin=df['Impact'].min(), vmax=df['Impact'].max()))
+                sm.set_array([])
+                cbar = plt.colorbar(sm, ax=ax, orientation='vertical', fraction=0.03, pad=0.04)
+                # cbar.set_label('Impact')
+                cbar.ax.tick_params(labelsize='small')
+                # cbar.ax.yaxis.label.set_color('white')  # Set colorbar label color to white
+                cbar.ax.yaxis.set_tick_params(color='white', labelcolor='white')  # Set colorbar ticks color to white
+                cbar.outline.set_edgecolor('black')  # Set colorbar border color
+                cbar.ax.set_facecolor('black')  # Set colorbar background color
+                cbar.ax.patch.set_edgecolor('black')  # Set colorbar border color
+
+                fig.subplots_adjust(right=0.85)  # Adjust the right side of the plot to make space for title
+                cbar.ax.text(0.7, 1.05, 'Impact', ha='center', va='center', color='white', fontsize='13', fontweight='bold', transform=cbar.ax.transAxes)
+            except Exception as e:
+                india_map.plot(ax=ax, color='gray', facecolor='none')
+
+            fig.set_facecolor('None')
+            ax.set_axis_off()
         else:
             chart = alt.Chart(incidents_by_drill_down).mark_bar().encode(
                 x='Counts:Q',
@@ -121,7 +151,10 @@ for tab, tab_name in zip(tabs, tab_names):
                 tooltip=[drill_down_basis, 'Counts']
             ).properties(width=700, height=300).interactive()
         
-        st.altair_chart(chart)
+        if drill_down_basis == 'State':
+            st.pyplot(fig)
+        else:
+            st.altair_chart(chart)
 
         # Visualization: Impact Heatmap based on Drill-Down Basis
         with st.expander("Visualise Impact Heatmap based on Drill-Down Basis"):
