cabe33534c82d771f3fc526ea1570b2cdf0556bf
ishan-surana <ishan5.mitmpl2022@learner.manipal.edu>
2024-09-02 02:14:16 +0530

    Created vanilla HTML, CSS, JS version

---
+++ vanilla/app.py
@@ -0,0 +1,143 @@
+# Make a flask app that returns a plot on route /map. it should be a geopandas plot of the world. I will give the plotting logic.
+import geopandas as gpd
+import pandas as pd
+import matplotlib.pyplot as plt
+import io
+import base64
+from flask import Flask, jsonify, request, render_template, send_from_directory
+import altair as alt
+import os
+import numpy as np
+
+plt.switch_backend('Agg')
+app = Flask(__name__)
+df = pd.read_csv('data/cyber_incidents.csv')
+india_map = gpd.read_file('data/map/Indian_states.shp')
+
+@app.route('/')
+def index():
+    return render_template('index.html', tabs=df.drop(columns=['Date', 'Impact']).columns.tolist())
+
+@app.route('/get_options')
+def get_options():
+    tab = request.args.get('tab').replace(' ', '_')
+    return jsonify({'options': df[tab].unique().tolist()})
+
+@app.route('/stats')
+def get_stats():
+    start_date, end_date = request.args.get('start_date'), request.args.get('end_date')
+    if start_date == '' or end_date == '':
+        filtered_df = df
+    else:
+        filtered_df = df[(pd.to_datetime(df['Date']) >= pd.to_datetime(start_date)) & (pd.to_datetime(df['Date']) <= pd.to_datetime(end_date))]
+    mean_impact = filtered_df['Impact'].mean()
+    median_impact = filtered_df['Impact'].median()
+    max_impact = float(filtered_df['Impact'].max())
+    min_impact = float(filtered_df['Impact'].min())
+    stats = {'mean_impact': mean_impact, 'median_impact': median_impact, 'max_impact': max_impact, 'min_impact': min_impact}
+    return jsonify(stats)
+
+@app.route('/get_incidents')
+def get_incidents():
+    n = int(request.args.get('n'))
+    data = df[['Date', 'State', 'Sector', 'Impact', 'Attack_Type']].sort_values('Impact', ascending=False).head(n)
+    data['Date'] = pd.to_datetime(data['Date']).dt.strftime('%Y-%m-%d')
+    return jsonify(data.to_dict(orient='records'))
+
+@app.route('/map')
+def map():
+    tab = request.args.get('tab').replace(' ', '_')
+    start_date, end_date = pd.to_datetime(request.args.get('start_date')), pd.to_datetime(request.args.get('end_date'))
+    if str(start_date) == 'NaT' or str(end_date) == 'NaT' or start_date == '' or end_date == '':
+        filtered_df = df
+    else:
+        filtered_df = df[(pd.to_datetime(df['Date']) >= pd.to_datetime(start_date)) & (pd.to_datetime(df['Date']) <= pd.to_datetime(end_date))]
+    if tab == 'State':
+        states = request.args.get('options')
+        states = states.split(',')
+        filtered_df = filtered_df[filtered_df['State'].isin(states)]
+
+        fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+        india_map.boundary.plot(ax=ax, color='black', facecolor='none')
+        try:
+            india_map.plot(ax=ax, color='white', facecolor='none')
+            merged_map = india_map.merge(filtered_df.rename(columns={'State': 'state'}).groupby('state').size().reset_index(name='Impact'), on='state').plot(ax=ax, column='Impact', cmap='YlOrRd', legend=False)
+            # Create a colorbar
+            sm = plt.cm.ScalarMappable(cmap='YlOrRd', norm=plt.Normalize(vmin=df['Impact'].min(), vmax=df['Impact'].max()))
+            sm.set_array([])
+            cbar = plt.colorbar(sm, ax=ax, orientation='vertical', fraction=0.03, pad=0.04)
+            # cbar.set_label('Impact')
+            cbar.ax.tick_params(labelsize='small')
+            # cbar.ax.yaxis.label.set_color('white')  # Set colorbar label color to white
+            cbar.ax.yaxis.set_tick_params(color='white', labelcolor='white')  # Set colorbar ticks color to white
+            cbar.outline.set_edgecolor('black')  # Set colorbar border color
+            cbar.ax.set_facecolor('black')  # Set colorbar background color
+            cbar.ax.patch.set_edgecolor('black')  # Set colorbar border color
+
+            fig.subplots_adjust(right=0.85)  # Adjust the right side of the plot to make space for title
+            cbar.ax.text(0.7, 1.05, 'Impact', ha='center', va='center', color='white', fontsize='13', fontweight='bold', transform=cbar.ax.transAxes)
+        except Exception as e:
+            print(e)
+            # Print line number of error
+            india_map.plot(ax=ax, color='gray', facecolor='none')
+
+        fig.set_facecolor('None')
+        ax.set_axis_off()
+
+        img = io.BytesIO()
+        plt.savefig(img, format='png', bbox_inches='tight', transparent=True)
+        img.seek(0)
+        plot_url = base64.b64encode(img.getvalue()).decode()
+        plt.close()
+        return jsonify({'plot_url': f'data:image/png;base64,{plot_url}'})
+    
+    if tab == 'Sector':
+        sectors = request.args.get('options')
+        sectors = sectors.split(',')
+        filtered_df = filtered_df[filtered_df['Sector'].isin(sectors)].groupby(tab).size().reset_index(name='Counts')
+        chart = alt.Chart(filtered_df).mark_arc(innerRadius=50).encode(
+            theta=alt.Theta(field="Counts", type="quantitative"),
+            color=alt.Color(field="Sector", type="nominal"),
+            tooltip=['Sector', 'Counts:Q'])
+    elif tab == 'Month':
+        months = request.args.get('options')
+        months = [int(month) for month in months.split(',')] if months else []
+        filtered_df = filtered_df[filtered_df['Month'].isin(months)].groupby(tab).size().reset_index(name='Counts')
+        chart = alt.Chart(filtered_df).mark_line(point=True).encode(
+            x=alt.X('Month:N', sort=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'], axis=alt.Axis(labelColor='white', titleColor='white', tickColor='white', domainColor='white')),
+            y=alt.Y('Counts:Q', axis=alt.Axis(labelColor='white', titleColor='white', tickColor='white', domainColor='white')),
+            tooltip=['Month', 'Counts:Q'])
+    elif tab == 'Year':
+        years = request.args.get('options')
+        years = [int(year) for year in years.split(',')] if years else []
+        filtered_df = filtered_df[filtered_df['Year'].isin(years)].groupby(tab).size().reset_index(name='Counts')
+        chart = alt.Chart(filtered_df).mark_line(point=True).encode(
+            x=alt.X('Year:N', sort='-x', axis=alt.Axis(labelColor='white', titleColor='white', tickColor='white', domainColor='white')),
+            y=alt.Y('Counts:Q', axis=alt.Axis(labelColor='white', titleColor='white', tickColor='white', domainColor='white')),
+            tooltip=['Year', 'Counts:Q'])
+    elif tab == 'Attack_Type':
+        attack_types = request.args.get('options')
+        attack_types = attack_types.split(',')
+        filtered_df = filtered_df[filtered_df['Attack_Type'].isin(attack_types)].groupby(tab).size().reset_index(name='Counts')
+        chart = alt.Chart(filtered_df).mark_bar().encode(
+            x=alt.X('Counts:Q', axis=alt.Axis(labelColor='white', titleColor='white', tickColor='white', domainColor='white')),
+            y=alt.Y('Attack_Type:N', sort='-x', axis=alt.Axis(labelColor='white', titleColor='white', tickColor='white', domainColor='white')),
+            color=alt.Color('Counts:Q', scale=alt.Scale(scheme='viridis')),
+            tooltip=['Attack_Type', 'Counts:Q'])
+
+    chart = chart.properties(width=700, height=300
+        ).configure(background='black'
+        ).configure_legend(labelColor='white', titleColor='white')
+
+    img = io.BytesIO()
+    chart.save('chart.png')
+    with open('chart.png', 'rb') as f:
+        img.write(f.read())
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode()
+    img.close()
+    os.remove('chart.png')
+    return jsonify({'plot_url': f'data:image/png;base64,{plot_url}'})
+
+if __name__ == '__main__':
+    app.run(debug=False)
---
+++ vanilla/static/scripts.js
@@ -0,0 +1,282 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const sidebar = document.getElementById('sidebar');
+    const sidebarToggle = document.getElementById('sidebar-toggle');
+    const navbar = document.getElementsByClassName('navbar');
+    const content = document.getElementsByClassName('content');
+
+    // Event listener for sidebar toggle
+    sidebarToggle.addEventListener('click', () => {
+        const isCollapsed = sidebar.classList.toggle('collapsed');
+        // toggle navbar shifted
+        navbar[0].classList.toggle('shifted');
+        content[0].classList.toggle('shifted');
+    });
+    
+    // use /stats endpoint with args start_date and end_date from the date inputs to get the sidebar content. arrange the 4 fields in {'mean_impact': mean_impact, 'median_impact': median_impact, 'max_impact': max_impact, 'min_impact': min_impact} in a 2x2 grid
+    const statsUrl = '/stats';
+    const mapUrl = '/map';
+    const dateInputs = document.querySelectorAll('input[type="date"]');
+    const startDateInput = dateInputs[0];
+    const endDateInput = dateInputs[1];
+
+    // Function to update the stats and map
+    var updateStatsAndMap = async () => {
+        const startDate = startDateInput.value;
+        const endDate = endDateInput.value;
+        const statsUrlWithParams = `${statsUrl}?start_date=${startDate}&end_date=${endDate}`;
+
+        try {
+            const response = await fetch(statsUrlWithParams);
+            const data = await response.json();
+            const summary = document.getElementById('summary');
+            summary.innerHTML = `
+                <div class="grid-item">Mean Impact: <b>${data.mean_impact}</b></div>
+                <div class="grid-item">Median Impact: <b>${data.median_impact}</b></div>
+                <div class="grid-item">Max Impact: <b>${data.max_impact}</b></div>
+                <div class="grid-item">Min Impact: <b>${data.min_impact}</b></div><br><br>
+            `;
+        } catch (error) {
+            console.error('Error fetching stats:', error);
+        }
+
+        // Update the map for current tab
+        const currentTab = document.querySelector('.navbar a.active');
+        const tabName = currentTab.getAttribute('data-tab');
+        const selectedOptions = Array.from(document.querySelectorAll(`#options-${tabName} input[type="checkbox"]`)).filter(cb => cb.checked).map(cb => cb.value);
+        const mapUrlWithParams = `${mapUrl}?tab=${tabName}&options=${selectedOptions.join(',')}&start_date=${startDate}&end_date=${endDate}`;
+        try {
+            const response = await fetch(mapUrlWithParams);
+            const mapData = await response.json();
+            const mapElement = document.getElementById(`map-${tabName}`);
+            mapElement.src = mapData.plot_url;
+        }
+        catch (error) {
+            console.error('Error fetching map data:', error);
+        }
+    };
+
+    // Event listener for date input changes
+    dateInputs.forEach(dateInput => {
+        dateInput.addEventListener('change', updateStatsAndMap);
+    });
+
+    // Load the default stats and map
+    updateStatsAndMap();
+
+    // Event listener for tab clicks
+    document.querySelectorAll('.navbar a').forEach(tab => {
+        tab.addEventListener('click', async event => {
+            event.preventDefault();
+            const tabName = event.target.getAttribute('data-tab');
+            // add active class to the clicked tab and remove it from the others
+            document.querySelectorAll('.navbar a').forEach(tab => {
+                tab.classList.remove('active');
+            });
+            event.target.classList.add('active');
+            await loadTabContent(tabName, optionsUrl, mapUrl);
+        });
+    });
+
+    // Event listener for slider input changes
+    const slider = document.getElementById('slider');
+    // change the text of submit button to 'Display top n incidents' where n is the value of the slider
+    const submitButton = document.getElementById('submit');
+    slider.addEventListener('input', () => {
+        submitButton.textContent = `Display top ${slider.value} incidents`;
+    }
+    );
+
+    // Event listener for submit button. goes to /get_incidents endpoint with args n from slider, makes a modal with the df returned
+    submitButton.addEventListener('click', async () => {
+        const n = slider.value;
+        const url = `/get_incidents?n=${n}`;
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            const newWindow = window.open('', '_blank', 'width=800,height=600');
+            console.log(data);
+            newWindow.document.write(`
+                <html>
+                <head>
+                    <title>Top Incidents</title>
+                    <style>
+                    /* Dark theme for the modal */
+                    body {
+                        background-color: #333;
+                        color: #fff;
+                    }
+                    table {
+                        border-collapse: collapse;
+                        width: 100%;
+                    }
+                    th, td {
+                        border: 1px solid #ddd;
+                        padding: 8px;
+                        text-align: left;
+                    }
+                    th {
+                        background-color: #333;
+                        color: #fff;
+                    }
+                    tr:nth-child(even) {
+                        background-color: #444;
+                    }
+                    tr:hover {
+                        background-color: #ddd;
+                        color: #000;
+                    }
+                    </style>
+                </head>
+                <body>
+                    <h2>Top Incidents</h2>
+                    <table>
+                        <tr>
+                            <th>Date</th>
+                            <th>State</th>
+                            <th>Sector</th>
+                            <th>Impact</th>
+                            <th>Attack Type</th>
+                        </tr>
+                        ${data.map(row => `
+                            <tr>
+                                <td>${row.Date}</td>
+                                <td>${row.State}</td>
+                                <td>${row.Sector}</td>
+                                <td>${row.Impact}</td>
+                                <td>${row.Attack_Type}</td>
+                            </tr>
+                        `).join('')}
+                    </table>
+                </body>
+                </html>
+            `);
+        } catch (error) {
+            console.error('Error fetching incidents:', error);
+        }
+    });
+
+    // Event listener for modal close button
+    const modal = document.getElementById('modal');
+    window.addEventListener('click', event => {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+
+    // Load the default tab
+    const defaultTab = document.querySelector('.navbar a');
+    if (defaultTab) {
+        defaultTab.click();
+    }
+});
+
+function loadTabContent(tabName, optionsUrl, mapUrl) {
+    const tabContentDiv = document.getElementById('tab-content');
+    tabContentDiv.innerHTML = '<p>Loading...</p>'; // Show loading message
+    const dateInputs = document.querySelectorAll('input[type="date"]');
+    const StartDate = dateInputs[0].value;
+    const EndDate = dateInputs[1].value;
+    fetch(`${optionsUrl}?tab=${tabName}`)
+        .then(response => response.json())
+        .then(data => {
+            let content = `
+                <u><h4>Drill-down view by ${tabName.replace('_', ' ')}:-</h4></u>
+                <div class="container">
+                    <div class="checkbox-group-container">
+                        <div class="checkbox-group" id="options-${tabName}"></div>
+                        <div class="button-group">
+                            <button id="select-all-${tabName}" class="select-button">Select All</button>
+                            <button id="clear-all-${tabName}" class="clear-button">Clear All</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="container map-container">
+                    <img id="map-${tabName}" src="" alt="Map">
+                </div>
+            `;
+            tabContentDiv.innerHTML = content;
+
+            const options = data.options;
+            const checkboxGroup = document.getElementById(`options-${tabName}`);
+            options.forEach(option => {
+                const label = document.createElement('label');
+                const checkbox = document.createElement('input');
+                checkbox.type = 'checkbox';
+                checkbox.value = option;
+                label.appendChild(checkbox);
+                label.appendChild(document.createTextNode(option));
+                checkboxGroup.appendChild(label);
+                checkbox.checked = true;
+            });
+
+            const checkboxes = document.querySelectorAll(`#options-${tabName} input[type="checkbox"]`);
+            const selectedOptions = Array.from(checkboxes).filter(cb => cb.checked).map(cb => cb.value);
+            return fetch(`${mapUrl}?tab=${tabName}&options=${selectedOptions.join(',')}&start_date=${StartDate}&end_date=${EndDate}`);
+        })
+        .then(mapResponse => mapResponse.json())
+        .then(mapData => {
+            const mapElement = document.getElementById(`map-${tabName}`);
+            mapElement.src = mapData.plot_url;
+            attachCheckboxListeners(tabName, mapUrl);
+        })
+        .catch(error => {
+            console.error('Error loading tab content:', error);
+        });
+}
+
+
+function attachCheckboxListeners(tabName) {
+    const checkboxes = document.querySelectorAll(`#options-${tabName} input[type="checkbox"]`);
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', async () => {
+            const selectedOptions = Array.from(checkboxes).filter(cb => cb.checked).map(cb => cb.value);
+            const dateInputs = document.querySelectorAll('input[type="date"]');
+            const StartDate = dateInputs[0].value;
+            const EndDate = dateInputs[1].value;
+            const url = `${mapUrl}?tab=${tabName}&options=${selectedOptions.join(',')}&start_date=${StartDate}&end_date=${EndDate}`;
+            try {
+                const response = await fetch(url);
+                const map_data = await response.json();
+                const mapElement = document.getElementById(`map-${tabName}`);
+                mapElement.src = map_data.plot_url;
+            } catch (error) {
+                console.error('Error fetching map data:', error);
+            }
+        });
+    });
+
+    document.getElementById(`select-all-${tabName}`).addEventListener('click', () => {
+        checkboxes.forEach(checkbox => {
+            checkbox.checked = true;
+        });
+        const selectedOptions = Array.from(checkboxes).filter(cb => cb.checked).map(cb => cb.value);
+        const dateInputs = document.querySelectorAll('input[type="date"]');
+        const StartDate = dateInputs[0].value;
+        const EndDate = dateInputs[1].value;
+        const url = `${mapUrl}?tab=${tabName}&options=${selectedOptions.join(',')}&start_date=${StartDate}&end_date=${EndDate}`;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                const mapElement = document.getElementById(`map-${tabName}`);
+                mapElement.src = data.plot_url;
+            });
+    });
+
+    document.getElementById(`clear-all-${tabName}`).addEventListener('click', () => {
+        checkboxes.forEach(checkbox => {
+            checkbox.checked = false;
+        });
+        const selectedOptions = Array.from(checkboxes).filter(cb => cb.checked).map(cb => cb.value);
+        const dateInputs = document.querySelectorAll('input[type="date"]');
+        const StartDate = dateInputs[0].value;
+        const EndDate = dateInputs[1].value;
+        const url = `${mapUrl}?tab=${tabName}&options=${selectedOptions.join(',')}start_date=${StartDate}&end_date=${EndDate}`;
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                const mapElement = document.getElementById(`map-${tabName}`);
+                mapElement.src = data.plot_url;
+            });
+    });
+}
